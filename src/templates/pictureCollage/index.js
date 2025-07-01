cae0c3fd4e1409621148d693fdf92834bb14dbb9
Ojaswin Kaushik <ojaswin.kaushik@oneimpression.io>
2025-07-01 09:40:09 +0530

    buttons in collage

--- src/templates/pictureCollage/index.js
+++ src/templates/pictureCollage/index.js
@@ -8,23 +8,116 @@ const pictureCollage = () => {
   const pictues = [
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234728/Screenshot_2025-06-29_064103_oorc8q.png",
-      classes: ["wide"],
+      properties: [
+        {
+          element: "button",
+          props: {
+            textContent: "About Us",
+          },
+          styles: {
+            left: "40%",
+            bottom: "26px",
+          },
+          classes: ["collage-btn", "collage-text"],
+        },
+        {
+          element: "button",
+          styles: { left: "40%", top: "26px" },
+          props: { textContent: "brand story" },
+          classes: ["collage-text", "collage-btn-2"],
+        },
+      ],
     },
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234729/Screenshot_2025-06-29_065538_u9oeab.png",
     },
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234729/Screenshot_2025-06-29_065538_u9oeab.png",
-      classes: ["wide"],
+      properties: [
+        {
+          element: "button",
+          props: {
+            textContent: "Our Story",
+          },
+          styles: {
+            left: "30%",
+            bottom: "26px",
+          },
+          classes: ["collage-btn", "collage-text"],
+        },
+        {
+          element: "button",
+          styles: { left: "30%", top: "26px" },
+          props: { textContent: "founder story" },
+          classes: ["collage-text", "collage-btn-2"],
+        },
+      ],
     },
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234728/Screenshot_2025-06-29_064151_uqayx7.png",
+      properties: [
+        {
+          element: "button",
+          props: {
+            textContent: "learn more",
+          },
+          styles: {
+            left: "9rem",
+            bottom: "3rem",
+          },
+          classes: ["collage-btn", "collage-text"],
+        },
+        {
+          element: "button",
+          styles: { left: "10rem", top: "5rem" },
+          props: { textContent: "why prebiotic ?" },
+          classes: ["collage-text", "collage-btn-2"],
+        },
+      ],
     },
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234729/Screenshot_2025-06-29_064312_ermy0h.png",
+      properties: [
+        {
+          element: "button",
+          props: {
+            textContent: "shop now",
+          },
+          styles: {
+            left: "9rem",
+            bottom: "3rem",
+          },
+          classes: ["collage-btn", "collage-text"],
+        },
+        {
+          element: "button",
+          styles: { left: "10rem", top: "8rem" },
+          props: { textContent: "product page" },
+          classes: ["collage-text", "collage-btn-2"],
+        },
+      ],
     },
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234729/Screenshot_2025-06-29_065608_khvrzn.png",
+      properties: [
+        {
+          element: "button",
+          props: {
+            textContent: "recipes",
+          },
+          styles: {
+            left: "5rem",
+            bottom: "3rem",
+          },
+          classes: ["collage-btn", "collage-text"],
+        },
+        {
+          element: "button",
+          styles: { left: "6rem", top: "6rem", maxWidth: "100px" },
+          props: { textContent: "if flavors could kill" },
+          classes: ["collage-text", "collage-btn-2"],
+        },
+      ],
     },
     {
       url: "https://res.cloudinary.com/dgkja1ija/image/upload/v1751234727/Screenshot_2025-06-29_065629_hpod2j.png",
@@ -44,13 +137,27 @@ const pictureCollage = () => {
   collageContainer.classList.add("bg-red-circular-gradient");
   collageContainer.classList.add("base-container");
 
-  pictues.forEach(({ url, classes = [] }, index) => {
+  pictues.forEach(({ url, properties = [] }, index) => {
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.style.height = "100%";
+    wrapperDiv.style.width = "100%";
+    wrapperDiv.style.position = "relative";
+    wrapperDiv.style.gridArea = `img${index + 1}`;
+
+    properties.forEach(({ element, styles = {}, props = {}, classes = [] }) => {
+      const elem = document.createElement(element);
+      Object.entries(props).forEach(([key, value]) => (elem[key] = value));
+      Object.entries(styles).forEach(
+        ([key, value]) => (elem.style[key] = value)
+      );
+      elem.classList.add(...classes);
+      wrapperDiv.append(elem);
+    });
     const img = document.createElement("img");
     img.src = url;
     img.alt = "image";
-    img.style.gridArea = `img${index + 1}`;
-    classes.forEach((item) => img.classList.add(item));
-    collageContainer.append(img);
+    wrapperDiv.append(img);
+    collageContainer.append(wrapperDiv);
   });
   app.appendChild(collageContainer);
 };
